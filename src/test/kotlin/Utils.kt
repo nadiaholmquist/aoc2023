5f05c194d24890d8a2091df04d1090dd3f2044fc
Nadia Holmquist Pedersen <nadia@nhp.sh>
2023-12-05 14:19:00 +0100

    Refactor test stuff a little

--- src/test/kotlin/Utils.kt
+++ src/test/kotlin/Utils.kt
@@ -2,17 +2,18 @@ import java.io.File
 import kotlin.test.assertEquals
 import kotlin.test.fail
 
+fun getTestInput(named: String): String {
+	val inputUrl = PuzzleTests::class.java.classLoader.getResource("${named}.txt") ?: fail()
+	return File(inputUrl.toURI()).readText().trimEnd()
+}
+
 fun test(day: Int, part: Int, expected: Int, inputFile: String? = null) {
 	val constructor = Class.forName("puzzles.Day$day").kotlin.constructors.first()
-	val inputName = inputFile ?: "day${day}_${part}"
-
-	val inputUrl = PuzzleTests::class.java.classLoader.getResource("${inputName}.txt") ?: fail()
-	val input = File(inputUrl.toURI()).readText().trimEnd()
-
+	val input = getTestInput(inputFile ?: "day${day}_${part}")
 	val puzzle = constructor.call(input) as Puzzle
 	val result = if (part == 1) puzzle.part1() else puzzle.part2()
 	if (result != expected) println("Input:\n$input")
-	assertEquals(result, expected)
+	assertEquals(expected, result)
 }
 
 fun testPart1(day: Int, expected: Int, inputFile: String? = null) =
@@ -23,10 +24,7 @@ fun testPart2(day: Int, expected: Int, inputFile: String? = null) =
 
 fun testBoth(day: Int, expected1: Int, expected2: Int, inputFile: String? = null) {
 	val constructor = Class.forName("puzzles.Day$day").kotlin.constructors.first()
-	val inputName = inputFile ?: "day${day}"
-
-	val inputUrl = PuzzleTests::class.java.classLoader.getResource("${inputName}.txt") ?: fail()
-	val input = File(inputUrl.toURI()).readText().trimEnd()
+	val input = getTestInput(inputFile ?: "day${day}")
 
 	val puzzle = constructor.call(input) as Puzzle
 	val result1 = puzzle.part1()
@@ -34,6 +32,6 @@ fun testBoth(day: Int, expected1: Int, expected2: Int, inputFile: String? = null
 
 	if (result1 != expected1 || result2 != expected2) println("Input:\n$input")
 
-	assertEquals(result1, expected1)
-	assertEquals(result2, expected2)
+	assertEquals(expected1, result1)
+	assertEquals(expected2, result2)
 }
